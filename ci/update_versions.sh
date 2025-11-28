a94b61efd2b28612ce5f31e8f14f1a64ebc95b98
Victor Morales <chipahuac@hotmail.com>
2025-11-28 11:20:35 -0800

    feat(ci): Introduce update_versions.sh script for version management
    This commit introduces a new bash script, `update_versions.sh`, to manage versions in the project. It synchronizes GitHub Action commit hashes used in the workflows with their respective upstream repositories.
    
    The script first checks if 'uv' is installed and installs it if necessary. Then, it upgrades 'uv' and sets up 'pre-commit' and 'autoupdate'. After that, it retrieves a list of GitHub Action action names used in the workflows, excluding those from exceptions (defined as `reviewdog/action-misspell`).
    
    For each action found, the commit hash corresponding to the latest tag is fetched and replaced in the respective `.github/workflows/*.yml` files. This ensures that the GitHub Actions use the correct version of their dependencies.

---
+++ ci/update_versions.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# SPDX-license-identifier: Apache-2.0
+##############################################################################
+# Copyright (c) 2025
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+
+set -o errexit
+set -o pipefail
+if [[ ${DEBUG:-false} == "true" ]]; then
+    set -o xtrace
+fi
+
+trap "make fmt" EXIT
+
+if ! command -v uv >/dev/null; then
+    curl -LsSf https://astral.sh/uv/install.sh | sh
+fi
+uv sync --upgrade
+uvx pre-commit autoupdate
+
+# Update GitHub Action commit hashes
+gh_actions=$(grep -r "uses: [A-Za-z0-9_.-]*/[\_a-z\-]*@" .github/ | sed 's/@.*//' | awk -F ': ' '{ print $3 }' | sort -u)
+exceptions=('reviewdog/action-misspell')
+for action in $gh_actions; do
+    if [[ ${exceptions[*]} =~ (^|[^[:alpha:]])$action([^[:alpha:]]|$) ]]; then
+        commit_hash=$(git ls-remote "https://github.com/$action" | grep 'refs/tags/[v]\?[0-9][0-9\.]*\^{}$' | sed 's|refs/tags/[vV]\?[\.]\?||g; s|\^{}$||g' | sort -u -k2 -V | tail -1 | awk '{ printf "%s # %s\n",$1,$2 }')
+    else
+        commit_hash=$(git ls-remote "https://github.com/$action" | grep 'refs/tags/[v]\?[0-9][0-9\.]*$' | sed 's|refs/tags/[vV]\?[\.]\?||g' | sort -u -k2 -V | tail -1 | awk '{ printf "%s # %s\n",$1,$2 }')
+    fi
+    # shellcheck disable=SC2267
+    grep -ElRZ "uses: $action@" .github/ | xargs -0 -l sed -i -e "s|uses: $action@.*|uses: $action@$commit_hash|g"
+done
